4e01913d6ac4c10f882fdde2cc8024bb93cf0007
Кагадий Андрей <kagadi.a@gmail.com>
2023-12-13 15:50:57 +0300

    modified:   models

--- edm/documents/models.py
+++ edm/documents/models.py
@@ -12,6 +12,47 @@ class Organization(models.Model):
     )
 
 
+class Numeration(models.Model):
+    """Модель для счетчиков"""
+
+    name = models.CharField(verbose_name="Наименование счетчика", max_length=50)
+    num = models.PositiveIntegerField(
+        verbose_name="Текущий номер", blank=False, null=False
+    )
+
+
+class Theme(models.Model):
+    """Модель для списка тем документа"""
+
+    name = models.CharField(
+        verbose_name="Тема документа", max_length=100, blank=False, null=False
+    )
+
+
+class Detail(models.Model):
+    """Модель для подробностей документа"""
+
+    name = models.CharField(
+        verbose_name="Подробно для документа", max_length=100, blank=False, null=False
+    )
+
+
+class Given(models.Model):
+    """Модель для типа передачи документа"""
+
+    name = models.CharField(
+        verbose_name="Тип передачи документа", max_length=100, blank=False, null=False
+    )
+
+
+class Implementers(models.Model):
+    """Модель для исполнителей документа"""
+
+    name = models.CharField(
+        verbose_name="ФИО исполнителя", max_length=50, blank=False, null=False
+    )
+
+
 class Document(models.Model):
     """Модель для создания документа"""
 
@@ -26,35 +67,86 @@ class Document(models.Model):
     doc_type = models.CharField(
         verbose_name="Тип документа",
         help_text="Укажите тип документа",
-        max_length=2,
+        max_length=5,
         choices=DOCUMENT_TYPE_CHOICES,
         blank=False,
     )
+    from_where = models.ForeignKey(
+        Organization,
+        related_name="org_from",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+    area = models.CharField(max_length=100, blank=True, null=True)
+    who = models.CharField(max_length=100, blank=True, null=True)
 
-    reg = models.ForeignKey(
-        Reg, related_name="doc_reg", on_delete=models.CASCADE, blank=True, null=True
+    input_num = models.PositiveIntegerField(
+        verbose_name="Входящий номер", blank=True, null=True
     )
-    title = models.CharField(max_length=100, blank=True, null=True)
-    file = models.FileField(upload_to="./docs/", storage=fs, blank=True, null=True)
-    size = models.IntegerField(blank=True, null=True)
-    description = models.CharField(max_length=500, blank=True, null=True)
-    date = models.DateTimeField(blank=True, null=True)
-    common = models.BooleanField(default=False)
-    signature = models.CharField(max_length=500, blank=True, null=True)
-    signature_end = models.BooleanField(default=False)
-    hash = models.ForeignKey(
-        Block, related_name="doc_h", on_delete=models.CASCADE, blank=True, null=True
+    output_num = models.PositiveIntegerField(
+        verbose_name="Исходящий номер", blank=True, null=True
     )
-    cancel_description = models.CharField(max_length=500, blank=True, null=True)
-    cancel_file = models.FileField(
-        upload_to="./cancel_docs/", storage=fs, blank=True, null=True
+    theme = models.ForeignKey(
+        Theme,
+        related_name="theme_doc",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
     )
-    file_cabinet = models.ForeignKey(
-        FileCabinet,
-        related_name="doc_fc",
-        on_delete=models.CASCADE,
+    detail = models.ForeignKey(
+        Detail,
+        related_name="detail_doc",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+    given = models.ForeignKey(
+        Given,
+        related_name="given_doc",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+    file = models.FileField(upload_to="docs/%Y/%m/%d/", blank=True, null=True)
+    size = models.PositiveIntegerField(blank=True, null=True)
+    description = models.TextField(
+        verbose_name="Прочее",
+        help_text="Укажите прочие подробности",
+        blank=True,
+        null=True,
+    )
+    implementers = models.ForeignKey(
+        Implementers,
+        related_name="implementers_doc",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
+    date_input = models.DateTimeField(
+        verbose_name="Дата входящего",
+        help_text="Укажите дату входящего документа",
+        blank=True,
+        null=True,
+    )
+    date_output = models.DateTimeField(
+        verbose_name="Дата исходящего",
+        help_text="Укажите дату исходящего документа",
         blank=True,
         null=True,
     )
 
-    post_delete.connect(receiver=delete_doc)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name="author_doc",
+        verbose_name="Автор создания",
+    )
+    modified_date = models.DateTimeField(
+        'Дата изменения',
+        auto_add=True,
+    )
+    created_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
