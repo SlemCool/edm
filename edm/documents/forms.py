fd182b4db69f6613424857a28a44d2e308f0f324
Кагадий Андрей <kagadi.a@gmail.com>
2023-12-20 15:39:38 +0300

    modified templates

--- edm/documents/forms.py
+++ edm/documents/forms.py
@@ -7,16 +7,30 @@ from .models import Document
 class DocumentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['doc_type'].empty_label = 'Выберите тип документа'
+        self.fields["doc_type"].empty_label = "Выберите тип документа"
 
     class Meta:
         model = Document
-        fields = ['doc_type', 'from_doc', 'description']
+        fields = [
+            "doc_type",
+            "from_doc",
+            "where_doc",
+            "area",
+            "who",
+            "theme",
+            "detail",
+            "given",
+            "file",
+            "description",
+            "implementers",
+            "date_input_doc",
+            "date_output_doc",
+        ]
 
     def clean_text(self):
-        description = self.cleaned_data['description']
-        if 'Трамп' in description:
-            raise ValidationError('Не допустимо упоминание Трампа в Документе!')
+        description = self.cleaned_data["description"]
+        if "Трамп" in description:
+            raise ValidationError("Не допустимо упоминание Трампа в Документе!")
         return description
 
 
